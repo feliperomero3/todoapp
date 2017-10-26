8068561705d9e780e77261a93cf44b7754045b7a
Felipe Romero <feliperomero3@outlook.com>
2017-10-25 18:39:50 -0600

    Update todo model and collection.

--- js/models/todo.js
+++ js/models/todo.js
@@ -67,7 +67,35 @@ $(function () {
     // --------------
   
     // The DOM element for a todo item...
-    
+    var TodoView = Backbone.View.extend({
+
+        // ... is a list tag.
+        tagName: "li",
+
+        // Cache the template function for a single item.
+        template: _.template($('#item-template').html()),
+
+        // The DOM events specific to an item.
+        events: {
+            "click .toggle"   : "toggleDone",
+            "dblclick .view"  : "edit",
+            "click a.destroy" : "clear",
+            "keypress .edit"  : "updateOnEnter",
+            "blur .edit"      : "close"
+        },
+
+        // The TodoView listens for changes to its model, re-rendering.
+        // Since there's a one-to-one correspondence between a **Todo**
+        // and a **TodoView** in this app, we set a direct reference
+        // on the model for convenience.
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+
+        // Re-render the titles of the todo item.
+        
+    });
 
 
     // The Application
